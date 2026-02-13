6882c7b4289e56118a95cdbe99e7952a4ea4aef1
Ismail Nyms <smailkindle4@gmail.com>
2026-02-13 23:11:50 +0100

    Refactor CORS setup and export app
    Updated CORS configuration and removed console log.

--- back/server.js
+++ back/server.js
@@ -14,19 +14,16 @@ const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
-// ✅ CORS (Express 5 compatible)
+//  CORS (Express 5 compatible)
 app.use(
   cors({
     origin: ['http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 
 // Body parsers
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -41,6 +38,4 @@ app.get("/", (req, res) => {
   res.send("Working");
 });
 
-app.listen(port, () => {
-  console.log("Listening on", port);
-});
+export default app;
