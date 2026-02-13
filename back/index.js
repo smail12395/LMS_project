e48b43fb1df5113d07524b3603dae47d2e372553
Ismail Nyms <smailkindle4@gmail.com>
2026-02-13 23:29:21 +0100

    Initialize Express server with routes and middleware
    Set up Express server with CORS and API routes

---
+++ back/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongoDb.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+import userRoute from "./routes/userRouters.js";
+import instructorRoute from "./routes/instructorRouters.js";
+import adminRouter from "./routes/adminRouters.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+//  CORS (Express 5 compatible)
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+  })
+);
+
+
+// Body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API routes
+app.use("/api/user", userRoute);
+app.use("/api/instructor", instructorRoute);
+app.use("/api/admin", adminRouter);
+
+
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+export default app;
